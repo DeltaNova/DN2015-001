41bb03a72e5479e48d8ec964a46205e84ad049a5
Matthew Tunstall <git@deltanova.co.uk>
2015-12-15 22:45:05 +0000

    Add outline for SSD1306 Library

---
+++ libraries/ssd1306/ssd1306.cpp
@@ -0,0 +1,6 @@
+// ssd1306.cpp - also see header ssd1306.h
+// Library for SSD1306 OLED Display Driver
+// Author: M. Tunstall
+
+#include "ssd1306.h"
+
---
+++ libraries/ssd1306/ssd1306.h
@@ -0,0 +1,10 @@
+// ssd1306.h - Header file for ssd1306.cpp
+// Library for SSD1306 OLED Display Driver
+// Author: M. Tunstall
+
+#ifndef SSD1306_H //If ssd1306.h hasn't been included elsewhere, define it now.
+#define SSD1306_H
+// Content goes here
+
+
+#endif // End of ifndef SSD1306_H
