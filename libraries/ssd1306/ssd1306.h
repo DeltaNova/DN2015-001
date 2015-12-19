efc374ced881c55b6879e6399a0fb572cf04d125
Matthew Tunstall <git@deltanova.co.uk>
2015-12-19 15:35:16 +0000

    Add SSD1306 library, link to screen_write.ino

--- libraries/ssd1306/ssd1306.h
+++ libraries/ssd1306/ssd1306.h
@@ -2,8 +2,9 @@
 // Library for SSD1306 OLED Display Driver
 // Author: M. Tunstall
 #include <stdint.h>         // Enable fixed width integers.
-
-
+//#include <Wire.h>           // TWI Library
+#include "twi.h"            // My wrapper around <Wire.h>
+#include <avr/pgmspace.h>
 #ifndef SSD1306_H //If ssd1306.h hasn't been included elsewhere, define it now.
 #define SSD1306_H
 
@@ -15,20 +16,187 @@
 //           functions. Aim is to make it easier to switch primary code
 //           and library to another i2c/twi library at a later date.
 
-
+template< typename TWI_T >
 class SSD1306
 {
     protected: // accessed by member functions and derived classes.
-
+        TWI_T TWI; // Reference to an instance of the TWI/I2C interface.
     private: // accessed by member functions but not derived classes.
 
     public: // accessed by anybody.
-        SSD1306() // Default Constructor
-        {
-
+        SSD1306(){ // Default Constructor
+            TWI.begin();
         }
         ~SSD1306(){}  // Destructor
+        void disp_setup(uint8_t OLED_ADDR);
+        void clear_buffer(uint8_t OLED_ADDR);
+        void setAddressMode(uint8_t OLED_ADDR, uint8_t mode);
+        void setCursor(uint8_t OLED_ADDR, uint8_t row_start = 0x00, uint8_t col_start = 0x00, uint8_t col_end = 0x7F, uint8_t row_end = 0x07);
+        void setPageStartCol(uint8_t OLED_ADDR, uint8_t pscol);
+        void PROGMEMwriteBuf(uint8_t OLED_ADDR, const uint8_t* buffer_to_write);
+        void writeLine(uint8_t OLED_ADDR, const uint8_t* buffer_name, uint8_t buffer_length, const uint8_t* symbol_buffer);
 
 };
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::disp_setup(uint8_t OLED_ADDR){
+    // Initialisation of OLED Display
+    TWI.beginTransmission(OLED_ADDR); // Start Transmitting
+    // Initialisation Based upon Application Note Example
+    TWI.write(0x00);                   // Send Command Byte Stream
+    // --
+    TWI.write(0xAE);                   // Turn Display Off
+    // ---
+    TWI.write(0xA8);                   // Set Multiplex Ratio
+    TWI.write(0x3f);                   // 1/64 duty cycle
+    // ---
+    TWI.write(0xD3);                   // Set Display Offset
+    TWI.write(0x00);                   // No offset applied
+    // ---
+    TWI.write(0x40);                   // Set Display Start Line #0
+    // ---
+    TWI.write(0xA1);                   // Set Segment Remap (Flips Display) A0/A1
+    // ---
+    TWI.write(0xC8);                   // COM Scan Direction c0/c8
+    // ---
+    TWI.write(0xDA);                   // Set COM pins
+    TWI.write(0x12);                   // 0x12 - See Application Note SW INIT
+    // ---
+    TWI.write(0x81);                   // Set Contrast
+    TWI.write(0x7F);                   // Default Contrast Level 127/255
+    // ---
+    TWI.write(0xA4);                   // Entire Display On - Output follows RAM Contents
+    // ---
+    TWI.write(0xA6);                   // Set Normal Display 0xA7 inverts
+    // ---
+    TWI.write(0xD5);                   // Display Clk Div
+    TWI.write(0x80);                   // Default Value 0x80
+    // ---
+    TWI.write(0x8D);                   // Setup Charge Pump - See SSD1306 Application Note
+    TWI.write(0x14);                   // Enable Charge Pump during display on.
+    // ---
+    TWI.write(0xD9);                   // Setup Precharge
+    TWI.write(0x22);                   //
+    // ---
+    TWI.write(0xDB);                   //VCOMH DESELECT
+    TWI.write(0x30);                   //
+    // ---
+    TWI.write(0x20);                   // Set Mem Addr Mode
+    TWI.write(0x00);                   // Horzontal
+
+    TWI.write(0xAF);                   // Display On
+    // ---
+    TWI.endTransmission(); // Stop Transmitting
+}
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::clear_buffer(uint8_t OLED_ADDR){
+    // Clear GFX Buffer
+    TWI.beginTransmission(OLED_ADDR);
+    TWI.write(0x00);   // Control Byte Command Stream
+    TWI.write(0x21);    // Setup Column Addresses
+    TWI.write(0x00);    // Col Start Addr
+    TWI.write(0x7F);    // Col End Addr
+    TWI.write(0x22);    // Set Page Addr
+    TWI.write(0x00);    // Start Page 0
+    TWI.write(0x07);    // End Page 7
+    TWI.endTransmission();
+
+    for (uint16_t i=0; i<1024; i++){
+        TWI.beginTransmission(OLED_ADDR);
+        TWI.write(0x40);      // Control Byte Data Stream
+        for (uint8_t x=0; x<16; x++) {
+            TWI.write(0x00);
+            i++;
+        }
+        i--;
+        TWI.endTransmission();
+    }
+}
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::setAddressMode(uint8_t OLED_ADDR, uint8_t mode){
+    // 0 = Horizontal (Default/Fallback)
+    // 1 = Vertical
+    // 2 = Page
+    TWI.beginTransmission(OLED_ADDR);
+    TWI.write(0x00);       // Control Byte Command Stream
+    TWI.write(0x20);       // Setup Address Mode
+    if (mode == 0){         // Setup Horizontal Mode
+        TWI.write(0x00);   // Horizontal
+    }
+    else if (mode == 1){    // Setup Vertical Mode
+        TWI.write(0x01);   // Vertical
+    }
+    else if (mode == 2){    // Setup Page Mode
+        TWI.write(0x02);   // Page
+    }
+    else{                   // Setup Horizontal Mode as fallback option
+        TWI.write(0x00);   // Horizontal
+    }
+    TWI.endTransmission();
+}
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::setCursor(uint8_t OLED_ADDR, uint8_t row_start, uint8_t col_start, uint8_t col_end, uint8_t row_end){
+    // Set Cursor Position
+    TWI.beginTransmission(OLED_ADDR);
+    TWI.write(0x00); // Control Byte Command Stream
+    TWI.write(0x21); // Set Column Address
+    TWI.write(col_start); // Start at column 0
+    TWI.write(col_end); // End at column 127
+    TWI.write(0x22); // Set Page (Row) address
+    TWI.write(row_start); // Start Page
+    TWI.write(row_end); // End Page
+    TWI.endTransmission();
+}
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::setPageStartCol(uint8_t OLED_ADDR, uint8_t pscol){
+    uint8_t upper_nibble = (((pscol & 0xF0)>>4)|0x10);
+    uint8_t lower_nibble = pscol & 0x0F;
+    TWI.beginTransmission(OLED_ADDR);
+    TWI.write(0x00); // Control Byte Command Stream
+    TWI.write(lower_nibble); // Set Lower Col Start Addr (Page Addr Mode)
+    TWI.write(upper_nibble); // Set Upper Col Start Addr (Page Addr Mode)
+    TWI.endTransmission();
+    }
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::PROGMEMwriteBuf(uint8_t OLED_ADDR, const uint8_t* buffer_to_write)
+{
+    for (uint16_t i=0; i<1024; i++){
+        TWI.beginTransmission(OLED_ADDR);
+        TWI.write(0x40);      // Control Byte Data Stream
+        for (uint8_t x=0; x<16; x++) {
+            TWI.write(pgm_read_byte(&buffer_to_write[i]));
+            i++;
+        }
+        i--;
+        TWI.endTransmission();
+    }
+}
+
+template< typename TWI_T >
+void SSD1306<TWI_T>::writeLine(uint8_t OLED_ADDR, const uint8_t* buffer_name, uint8_t buffer_length, const uint8_t* symbol_buffer){
+    // Write Text Display Line
+    // Note: Set cursor position before writing.
+
+    // For each item in data buffer
+    for (uint16_t i=0; i<buffer_length; i++){
+        TWI.beginTransmission(OLED_ADDR);
+        TWI.write(0x40);                   // Control Byte Data Stream
+        // Multiply by 6 to get char start position in the ascii_buffer
+        uint16_t offset = pgm_read_byte(&buffer_name[i]) * 6;
+        // Print each byte that makes up the character
+        for ( uint16_t x =0; x<6; x++) {
+            TWI.write(pgm_read_byte(&symbol_buffer[offset+x]));
+        }
+        TWI.endTransmission();
+    }
+    // Note: Reset the cursor position to Row 0, Col 0. Otherwise the
+    //       buffers become offset on the next loop.
+}
+
 
 #endif // End of ifndef SSD1306_H
--- screen_write/screen_write.ino
+++ screen_write/screen_write.ino
@@ -25,6 +25,7 @@
 #include <Wire.h> // Arduino Library with Serial & I2C interfaces
 #include <stdint.h>         // Enable fixed width integers.
 #include "ascii_buffer.h" // Include 'buffer' ASCII Chars & Symbols
+#include "ssd1306.h"
 #define OLED_ADDR 0x3C // Address of I2C OLED Display
 
 // ---------------------------------------------------------------------
@@ -183,150 +184,122 @@ const uint8_t buffer3[1024] PROGMEM = {
     0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
-void disp_setup();
-void draw_buffer();
-void draw_buffer2();
-void clear_buffer();
-void setCursor(uint8_t, uint8_t, uint8_t, uint8_t);
+//void disp_setup();
+//void draw_buffer();
+//void draw_buffer2();
+//void clear_buffer();
+//void setCursor(uint8_t, uint8_t, uint8_t, uint8_t);
 
 class I2C {
+    // Specific to Atmega328p
     public:
-        I2C(){}
-        void begin(){
-            Wire.begin();
-        }
+        I2C(){} // Constructor
+        ~I2C(){} // Destructor
+        void begin(){ Wire.begin(); }
+        void beginTransmission(uint8_t addr){ Wire.beginTransmission(addr); }
+        void write(uint8_t byte){ Wire.write(byte); }
+        void endTransmission(){ Wire.endTransmission(); }
+        void setSCLfreq(uint8_t TWBRbyte){TWBR = TWBRbyte;};
 };
 
-I2C TWI;
-
+I2C i2c; // Instance of I2C class
+typedef I2C TWI;
+SSD1306<TWI> OLED;
 // ---------------------------------------------------------------------
 void setup() {
-    //I2C TWI{};
-    //Serial.begin(9600); // Create a serial connection
-    TWI.begin();       // Join I2C Bus as Master
+    i2c.begin();       // Join I2C Bus as Master
     // Set I2C to 400KHz mode
-    TWBR=12;
-    disp_setup();       // Setup Display
-}
-
-void disp_setup(){
-    // Initialisation of OLED Display
-    Wire.beginTransmission(OLED_ADDR); // Start Transmitting
-    //Wire.write(0x00);                   // Low Col = 0
-    //Wire.write(0x10);                   // Hi Col = 0
-
-    // ---
-    //Wire.write(0x20);                   // Set Mem Addr Mode
-    //Wire.write(0x00);                   // 0x00 Horizontal
-
-    // Initialisation Based upon Application Note Example
-    Wire.write(0x00);                   // Send Command Byte Stream
-    // --
-    Wire.write(0xAE);                   // Turn Display Off
-    // ---
-    Wire.write(0xA8);                   // Set Multiplex Ratio
-    Wire.write(0x3f);                   // 1/64 duty cycle
-    // ---
-    Wire.write(0xD3);                   // Set Display Offset
-    Wire.write(0x00);                   // No offset applied
-    // ---
-    Wire.write(0x40);                   // Set Display Start Line #0
-    // ---
-    Wire.write(0xA1);                   // Set Segment Remap (Flips Display) A0/A1
-    // ---
-    Wire.write(0xC8);                   // COM Scan Direction c0/c8
-    // ---
-    Wire.write(0xDA);                   // Set COM pins
-    Wire.write(0x12);                   // 0x12 - See Application Note SW INIT
-    // ---
-    Wire.write(0x81);                   // Set Contrast
-    Wire.write(0x7F);                   // Default Contrast Level 127/255
-    // ---
-    Wire.write(0xA4);                   // Entire Display On - Output follows RAM Contents
-    // ---
-    Wire.write(0xA6);                   // Set Normal Display 0xA7 inverts
-    // ---
-    Wire.write(0xD5);                   // Display Clk Div
-    Wire.write(0x80);                   // Default Value 0x80
-    // ---
-    Wire.write(0x8D);                   // Setup Charge Pump - See SSD1306 Application Note
-    Wire.write(0x14);                   // Enable Charge Pump during display on.
-    // ---
-    Wire.write(0xD9);                   // Setup Precharge
-    Wire.write(0x22);                   //
-    // ---
-    Wire.write(0xDB);                   //VCOMH DESELECT
-    Wire.write(0x30);                   //
-    // ---
-    Wire.write(0x20);                   // Set Mem Addr Mode
-    Wire.write(0x00);                   // Horzontal
-
-
-    Wire.write(0xAF);                   // Display On
-    // ---
-    Wire.endTransmission(); // Stop Transmitting
+    i2c.setSCLfreq(12);
+    OLED.disp_setup(OLED_ADDR);       // Setup Display
 }
 /*
-void draw_buffer(){
-    // Draw buffer on display
-    Wire.beginTransmission(OLED_ADDR);
-    Wire.write(0x00);    // Control Byte Command Stream
-    Wire.write(0x21);    // Setup Column Addresses
-    Wire.write(0x00);    // Col Start Addr
-    Wire.write(0x7F);    // Col End Addr
-    Wire.write(0x22);    // Set Page Addr
-    Wire.write(0x00);    // Start Page 0
-    Wire.write(0x07);    // End Page 7
-    Wire.endTransmission();
+void disp_setup(){
+    // Initialisation of OLED Display
+    i2c.beginTransmission(OLED_ADDR); // Start Transmitting
 
-    for (uint16_t i=0; i<1024; i++){
-        Wire.beginTransmission(OLED_ADDR);
-        Wire.write(0x40);      // Control Byte Data Stream
-        for (uint8_t x=0; x<16; x++) {
-            Wire.write(buffer[i]);
-            i++;
-        }
-        i--;
-        Wire.endTransmission();
-    }
+    // Initialisation Based upon Application Note Example
+    i2c.write(0x00);                   // Send Command Byte Stream
+    // --
+    i2c.write(0xAE);                   // Turn Display Off
+    // ---
+    i2c.write(0xA8);                   // Set Multiplex Ratio
+    i2c.write(0x3f);                   // 1/64 duty cycle
+    // ---
+    i2c.write(0xD3);                   // Set Display Offset
+    i2c.write(0x00);                   // No offset applied
+    // ---
+    i2c.write(0x40);                   // Set Display Start Line #0
+    // ---
+    i2c.write(0xA1);                   // Set Segment Remap (Flips Display) A0/A1
+    // ---
+    i2c.write(0xC8);                   // COM Scan Direction c0/c8
+    // ---
+    i2c.write(0xDA);                   // Set COM pins
+    i2c.write(0x12);                   // 0x12 - See Application Note SW INIT
+    // ---
+    i2c.write(0x81);                   // Set Contrast
+    i2c.write(0x7F);                   // Default Contrast Level 127/255
+    // ---
+    i2c.write(0xA4);                   // Entire Display On - Output follows RAM Contents
+    // ---
+    i2c.write(0xA6);                   // Set Normal Display 0xA7 inverts
+    // ---
+    i2c.write(0xD5);                   // Display Clk Div
+    i2c.write(0x80);                   // Default Value 0x80
+    // ---
+    i2c.write(0x8D);                   // Setup Charge Pump - See SSD1306 Application Note
+    i2c.write(0x14);                   // Enable Charge Pump during display on.
+    // ---
+    i2c.write(0xD9);                   // Setup Precharge
+    i2c.write(0x22);                   //
+    // ---
+    i2c.write(0xDB);                   //VCOMH DESELECT
+    i2c.write(0x30);                   //
+    // ---
+    i2c.write(0x20);                   // Set Mem Addr Mode
+    i2c.write(0x00);                   // Horzontal
+
+
+    i2c.write(0xAF);                   // Display On
+    // ---
+    i2c.endTransmission(); // Stop Transmitting
 }
-*/
 
 void clear_buffer(){
     // Clear GFX Buffer
-    Wire.beginTransmission(OLED_ADDR);
-    Wire.write(0x00);   // Control Byte Command Stream
-    Wire.write(0x21);    // Setup Column Addresses
-    Wire.write(0x00);    // Col Start Addr
-    Wire.write(0x7F);    // Col End Addr
-    Wire.write(0x22);    // Set Page Addr
-    Wire.write(0x00);    // Start Page 0
-    Wire.write(0x07);    // End Page 7
-    Wire.endTransmission();
+    i2c.beginTransmission(OLED_ADDR);
+    i2c.write(0x00);   // Control Byte Command Stream
+    i2c.write(0x21);    // Setup Column Addresses
+    i2c.write(0x00);    // Col Start Addr
+    i2c.write(0x7F);    // Col End Addr
+    i2c.write(0x22);    // Set Page Addr
+    i2c.write(0x00);    // Start Page 0
+    i2c.write(0x07);    // End Page 7
+    i2c.endTransmission();
 
     for (uint16_t i=0; i<1024; i++){
-        Wire.beginTransmission(OLED_ADDR);
-        Wire.write(0x40);      // Control Byte Data Stream
+        i2c.beginTransmission(OLED_ADDR);
+        i2c.write(0x40);      // Control Byte Data Stream
         for (uint8_t x=0; x<16; x++) {
-            Wire.write(0x00);
+            i2c.write(0x00);
             i++;
         }
         i--;
-        Wire.endTransmission();
+        i2c.endTransmission();
     }
 }
 
 void PROGMEMwriteBuf(const uint8_t* buffer_to_write)
 {
     for (uint16_t i=0; i<1024; i++){
-        Wire.beginTransmission(OLED_ADDR);
-        Wire.write(0x40);      // Control Byte Data Stream
+        i2c.beginTransmission(OLED_ADDR);
+        i2c.write(0x40);      // Control Byte Data Stream
         for (uint8_t x=0; x<16; x++) {
-            Wire.write(pgm_read_byte(&buffer_to_write[i]));
+            i2c.write(pgm_read_byte(&buffer_to_write[i]));
             i++;
         }
         i--;
-        Wire.endTransmission();
+        i2c.endTransmission();
     }
 }
 
@@ -336,15 +309,15 @@ void writeLine(const uint8_t* buffer_name, uint8_t buffer_length){
 
     // For each item in data buffer
     for (uint16_t i=0; i<buffer_length; i++){
-        Wire.beginTransmission(OLED_ADDR);
-        Wire.write(0x40);                   // Control Byte Data Stream
+        i2c.beginTransmission(OLED_ADDR);
+        i2c.write(0x40);                   // Control Byte Data Stream
         // Multiply by 6 to get char start position in the ascii_buffer
         uint16_t offset = pgm_read_byte(&buffer_name[i]) * 6;
         // Print each byte that makes up the character
         for ( uint16_t x =0; x<6; x++) {
-            Wire.write(pgm_read_byte(&buffer[offset+x]));
+            i2c.write(pgm_read_byte(&buffer[offset+x]));
         }
-        Wire.endTransmission();
+        i2c.endTransmission();
     }
     // Note: Reset the cursor position to Row 0, Col 0. Otherwise the
     //       buffers become offset on the next loop.
@@ -352,65 +325,65 @@ void writeLine(const uint8_t* buffer_name, uint8_t buffer_length){
 
 void setCursor(uint8_t row_start = 0x00, uint8_t col_start = 0x00, uint8_t col_end = 0x7F, uint8_t row_end = 0x07){
     // Set Cursor Position
-    Wire.beginTransmission(OLED_ADDR);
-    Wire.write(0x00); // Control Byte Command Stream
-    Wire.write(0x21); // Set Column Address
-    Wire.write(col_start); // Start at column 0
-    Wire.write(col_end); // End at column 127
-    Wire.write(0x22); // Set Page (Row) address
-    Wire.write(row_start); // Start Page
-    Wire.write(row_end); // End Page
-    Wire.endTransmission();
+    i2c.beginTransmission(OLED_ADDR);
+    i2c.write(0x00); // Control Byte Command Stream
+    i2c.write(0x21); // Set Column Address
+    i2c.write(col_start); // Start at column 0
+    i2c.write(col_end); // End at column 127
+    i2c.write(0x22); // Set Page (Row) address
+    i2c.write(row_start); // Start Page
+    i2c.write(row_end); // End Page
+    i2c.endTransmission();
 }
 
 void setPageStartCol(uint8_t pscol){
     uint8_t upper_nibble = (((pscol & 0xF0)>>4)|0x10);
     uint8_t lower_nibble = pscol & 0x0F;
-    Wire.beginTransmission(OLED_ADDR);
-    Wire.write(0x00); // Control Byte Command Stream
-    Wire.write(lower_nibble); // Set Lower Col Start Addr (Page Addr Mode)
-    Wire.write(upper_nibble); // Set Upper Col Start Addr (Page Addr Mode)
-    Wire.endTransmission();
+    i2c.beginTransmission(OLED_ADDR);
+    i2c.write(0x00); // Control Byte Command Stream
+    i2c.write(lower_nibble); // Set Lower Col Start Addr (Page Addr Mode)
+    i2c.write(upper_nibble); // Set Upper Col Start Addr (Page Addr Mode)
+    i2c.endTransmission();
     }
 
 void setAddressMode(uint8_t mode){
     // 0 = Horizontal (Default/Fallback)
     // 1 = Vertical
     // 2 = Page
-    Wire.beginTransmission(OLED_ADDR);
-    Wire.write(0x00);       // Control Byte Command Stream
-    Wire.write(0x20);       // Setup Address Mode
+    i2c.beginTransmission(OLED_ADDR);
+    i2c.write(0x00);       // Control Byte Command Stream
+    i2c.write(0x20);       // Setup Address Mode
     if (mode == 0){         // Setup Horizontal Mode
-        Wire.write(0x00);   // Horizontal
+        i2c.write(0x00);   // Horizontal
     }
     else if (mode == 1){    // Setup Vertical Mode
-        Wire.write(0x01);   // Vertical
+        i2c.write(0x01);   // Vertical
     }
     else if (mode == 2){    // Setup Page Mode
-        Wire.write(0x02);   // Page
+        i2c.write(0x02);   // Page
     }
     else{                   // Setup Horizontal Mode as fallback option
-        Wire.write(0x00);   // Horizontal
+        i2c.write(0x00);   // Horizontal
     }
-    Wire.endTransmission();
+    i2c.endTransmission();
 }
-
+*/
 void loop(){
     //draw_buffer();
-    PROGMEMwriteBuf(buffer);  // Write data to display buffer from program memory
+    OLED.PROGMEMwriteBuf(OLED_ADDR,ascii_buffer);  // Write data to display buffer from program memory
     delay(2000);
-    PROGMEMwriteBuf(buffer2);
+    OLED.PROGMEMwriteBuf(OLED_ADDR,buffer2);
     delay(2000);
-    PROGMEMwriteBuf(buffer3);
+    OLED.PROGMEMwriteBuf(OLED_ADDR,buffer3);
     delay(2000);
-    clear_buffer();     // Clear display buffer
+    OLED.clear_buffer(OLED_ADDR);     // Clear display buffer
     delay(2000);
-    setCursor(0,0);
-    writeLine(databuffer,12);
-    setCursor(2,20);
-    writeLine(databuffer,12);
-    setCursor(4,40);
-    writeLine(databuffer,12);
+    OLED.setCursor(OLED_ADDR,0,0);
+    OLED.writeLine(OLED_ADDR,databuffer,12,ascii_buffer);
+    OLED.setCursor(OLED_ADDR,2,20);
+    OLED.writeLine(OLED_ADDR,databuffer,12,ascii_buffer);
+    OLED.setCursor(OLED_ADDR,4,40);
+    OLED.writeLine(OLED_ADDR,databuffer,12,ascii_buffer);
     // DEV Note:
     // When setting the cursor location for this last item it is with the
     // knowledge that there is too much text to write than columns available on screen.
@@ -418,9 +391,9 @@ void loop(){
     // data begins to overwrite itself from the staring column.
     // In this case the data is allowed to continue onto the next page
     // but it begins to write to this page at the same starting column as before.
-    setCursor(6,80,127,7);
-    writeLine(databuffer,12);
-    setCursor(); // Reset Cursor
+    OLED.setCursor(OLED_ADDR,6,80,127,7);
+    OLED.writeLine(OLED_ADDR,databuffer,12,ascii_buffer);
+    OLED.setCursor(OLED_ADDR); // Reset Cursor
 
     delay(3000);
 }
