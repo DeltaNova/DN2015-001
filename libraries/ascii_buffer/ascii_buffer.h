564d6be8491c0a448f2c58ff9eebebe2d54fc069
Matthew Tunstall <git@deltanova.co.uk>
2015-12-20 16:16:06 +0000

    Add header guards to ascii_buffer.h

--- libraries/ascii_buffer/ascii_buffer.h
+++ libraries/ascii_buffer/ascii_buffer.h
@@ -1,4 +1,38 @@
-const uint8_t ascii_buffer [1024] PROGMEM = { // 128 x 64 ASCII Symbol&Text Buffer
+// ascii_buffer.h - SSD1306 OLED Display buffer
+// Author: M. Tunstall
+/*
+  Copyright (C) 2015  Matthew Tunstall
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>         // Enable fixed width integers.
+#include <avr/pgmspace.h>
+
+// USAGE: Place '#define AVR_PROGMEM' in main code to store the buffer in program memory instead of sram
+
+#ifndef ASCII_BUFFER_H
+#define ASCII_BUFFER_H
+
+// 128 x 64 ASCII Symbol&Text Buffer
+#ifdef AVR_PROGMEM
+    const uint8_t ascii_buffer[1024] PROGMEM = {
+#endif
+
+#ifndef AVR_PROGMEM
+    const uint8_t ascii_buffer[1024] = {
+#endif
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     // 000 - NULL
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     // 001 -
     0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55,     // 002 -
@@ -197,3 +231,4 @@ const uint8_t ascii_buffer [1024] PROGMEM = { // 128 x 64 ASCII Symbol&Text Buff
 
     0xAA, 0x55, 0xAA, 0x55                  // Additional Unused Bytes
 };
+#endif // End ASCII_BUFFER_H
